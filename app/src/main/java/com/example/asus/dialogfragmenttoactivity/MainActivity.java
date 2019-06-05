fbcd4ad6e3963ba5782c04b6e88adb76d36f8a3e
Erick <erkhgns@gmail.com>
2019-06-05 14:53:12 +0800

    New toast

--- app/src/main/java/com/example/asus/dialogfragmenttoactivity/MainActivity.java
+++ app/src/main/java/com/example/asus/dialogfragmenttoactivity/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements CustomDialogFragment.OnInputListener {
 
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity implements CustomDialogFragm
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(this, "new changes", Toast.LENGTH_SHORT).show();
         openDialog  = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
 
